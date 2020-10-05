806d65c406a7783c4cad0f51d00b5bc020d4bf38
Ankit Gupta <ankitgupta.oct21@gmail.com>
2020-10-05 20:45:15 +0530

    Adds: private key and public key generator

---
+++ index.js
@@ -0,0 +1,35 @@
+const envConfig = require('./config/envConfig');
+const { generateChildNode, generateXPriv, generateXPub } = require('./src/extendedKeysGenerator/generateExtendedKeys');
+const { generatePrivateKey, generatePublicKey } = require('./src/keysGenerator/generateKeys');
+const generateWalletAddress = require('./src/addressGenerator/generateAddress');
+const { isValidMnemonic, isValidPath } = require('./src/validator/validate');
+
+const derivationPath = envConfig.get('derivation_path');
+const mnemonic = envConfig.get('mnemonic');
+const coin = envConfig.get('coin_symbol');
+const environment = envConfig.get('blockchain_environment');
+
+const main = (mnemonic, path, coin) => {
+  const isEthClone = (coin === 'ETH' || coin === 'ETC');
+
+  const childNode = generateChildNode(mnemonic, path);
+  const xprivChildNode = generateXPriv(childNode);
+  const xpubChildNode = generateXPub(childNode);
+  const privateKey = generatePrivateKey(xprivChildNode);
+  const publicKey = generatePublicKey(xpubChildNode);
+  const walletAddress = generateWalletAddress(privateKey, publicKey, environment, coin);
+
+  console.log('=================================================================');
+  console.log('Extended Private Key:', xprivChildNode);
+  console.log('Extended Public Key:', xpubChildNode);
+  console.log('Private Key:', isEthClone ? `0x${privateKey}` : privateKey);
+  console.log('Public Key:', isEthClone ? `0x${publicKey}` : publicKey);
+  console.log('Wallet Address:', walletAddress);
+  console.log('=================================================================');
+}
+
+if (isValidMnemonic(mnemonic) && isValidPath(derivationPath)) {
+  main(mnemonic, derivationPath, coin);
+} else {
+  throw new Error('Error in Mnemonic or Derivation Path')
+}
